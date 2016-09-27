aaad0bb6f7a639b9f291d01fe99580874ee20dcd
Andreas Buff <andreas.buff@buffhaltestelle.de>
2016-09-27 12:55:14 +0200

    initial commit

---
+++ BuffstopToolbox/BuffstopToolbox.h
@@ -0,0 +1,19 @@
+//
+//  BuffstopToolbox.h
+//  BuffstopToolbox
+//
+//  Created by Andreas Buff on 20/09/16.
+//  Copyright © 2016 buffhaltestelle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BuffstopToolbox.
+FOUNDATION_EXPORT double BuffstopToolboxVersionNumber;
+
+//! Project version string for BuffstopToolbox.
+FOUNDATION_EXPORT const unsigned char BuffstopToolboxVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BuffstopToolbox/PublicHeader.h>
+
+
