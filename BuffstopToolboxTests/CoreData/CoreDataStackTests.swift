3513df36c8ceccf93b2f77b0addcee68f46f3ace
Andreas Buff <autobuff@gmx.de>
2016-10-04 15:27:52 +0200

    adds changes forgotten to commit in previous commit "adds tests"

--- BuffstopToolboxTests/CoreData/CoreDataStackTests.swift
+++ BuffstopToolboxTests/CoreData/CoreDataStackTests.swift
@@ -8,28 +8,119 @@
 
 import XCTest
 
+import Foundation
+import CoreData
+@testable import BuffstopToolbox
+
 class CoreDataStackTests: XCTestCase {
+    
+    var coreDataStack = TestCoreDataStack()
+    let testEntity = "TestEntity"
     
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        coreDataStack = TestCoreDataStack()
     }
     
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
+    // MARK: - FetchAll
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    func testMainContextCreateAndFetchEntityAsync() {
+        let expectation = self.expectation(description: "returns")
+        let moc = coreDataStack.mainContext()
+        moc.perform {
+            //createe entity
+            let uuid = UUID().uuidString
+            let createe = TestEntity(context: moc)
+            createe.testStringAtribute = uuid
+            self.coreDataStack.save()
+            
+            //fetch entity
+            moc.perform({ 
+                let fetchRequest:NSFetchRequest<TestEntity> = TestEntity.fetchRequest()
+                fetchRequest.predicate = NSPredicate(format: "testStringAtribute == %@", uuid)
+                let fetchedResults: [TestEntity] = try! fetchRequest.execute()
+                XCTAssertTrue(fetchedResults.count == 1, "unique object can be fetched")
+                let fetchedObject = fetchedResults.first! 
+                XCTAssertEqual(fetchedObject.testStringAtribute, uuid)
+                expectation.fulfill()
+            })
         }
+
+        waitForExpectations(timeout: xcTestCaseDefaultTimeout)
     }
     
+    func testMainContextCreateAndFetchEntitySync() {
+        let uuid = UUID().uuidString
+        let moc = coreDataStack.mainContext()
+        moc.performAndWait {
+            //createe entity
+            
+            let createe = TestEntity(context: moc)
+            createe.testStringAtribute = uuid
+            self.coreDataStack.save()
+            
+        }
+        
+        var fetchedObject:TestEntity?
+        //fetch entity
+        moc.performAndWait {
+            let fetchRequest:NSFetchRequest<TestEntity> = TestEntity.fetchRequest()
+            fetchRequest.predicate = NSPredicate(format: "testStringAtribute == %@", uuid)
+            let fetchedResults: [TestEntity] = try! fetchRequest.execute()
+            XCTAssertTrue(fetchedResults.count == 1, "unique object can be fetched")
+            fetchedObject = fetchedResults.first!
+        }
+        XCTAssertEqual(fetchedObject?.testStringAtribute, uuid)
+    }
+    
+    func testBackgroundContextCreateAndFetchEntityAsync() {
+        let expectation = self.expectation(description: "returns")
+        let mainMoc = coreDataStack.mainContext()
+        let backgroundMoc = coreDataStack.newBackgroundContext()
+        backgroundMoc.perform {
+            //createe entity
+            let uuid = UUID().uuidString
+            let createe = TestEntity(context: backgroundMoc)
+            createe.testStringAtribute = uuid
+            try! backgroundMoc.save()
+            
+            //fetch entity
+            mainMoc.perform({
+                let fetchRequest:NSFetchRequest<TestEntity> = TestEntity.fetchRequest()
+                fetchRequest.predicate = NSPredicate(format: "testStringAtribute == %@", uuid)
+                let fetchedResults: [TestEntity] = try! fetchRequest.execute()
+                XCTAssertTrue(fetchedResults.count == 1, "unique object can be fetched")
+                let fetchedObject = fetchedResults.first!
+                XCTAssertEqual(fetchedObject.testStringAtribute, uuid)
+                expectation.fulfill()
+            })
+        }
+        
+        waitForExpectations(timeout: xcTestCaseDefaultTimeout)
+    }
+    
+    func testBackgroundContextCreateAndFetchEntitySync() {
+        let uuid = UUID().uuidString
+        let mainMoc = coreDataStack.mainContext()
+        let backgroundMoc = coreDataStack.newBackgroundContext()
+        backgroundMoc.performAndWait {
+            //createe entity
+            
+            let createe = TestEntity(context: backgroundMoc)
+            createe.testStringAtribute = uuid
+            try! backgroundMoc.save()
+            
+        }
+        
+        var fetchedObject:TestEntity?
+        //fetch entity
+        mainMoc.performAndWait {
+            let fetchRequest:NSFetchRequest<TestEntity> = TestEntity.fetchRequest()
+            fetchRequest.predicate = NSPredicate(format: "testStringAtribute == %@", uuid)
+            let fetchedResults: [TestEntity] = try! fetchRequest.execute()
+            XCTAssertTrue(fetchedResults.count == 1, "unique object can be fetched")
+            fetchedObject = fetchedResults.first!
+        }
+        XCTAssertEqual(fetchedObject?.testStringAtribute, uuid)
+    }
 }
