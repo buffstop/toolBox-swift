1b225e98e16e00687b161e5a2ba6abb425ec2a34
Andreas Buff <autobuff@gmx.de>
2016-10-08 21:50:13 +0200

    comments code made useless by Swift3 CoreData improvements

--- BuffstopToolbox/CoreData/CoreDataHelper.swift
+++ BuffstopToolbox/CoreData/CoreDataHelper.swift
@@ -11,20 +11,21 @@ import CoreData
 
 public class CoreDataHelper {
     
-    static public func fetchAll<T:NSManagedObject>(ofManagedObjectContext moc: NSManagedObjectContext,  completionHandler: @escaping (_ fetchedResult: [T]?, _ error: Error?) -> Void) {
-        moc.perform {
-            let fetchRequest = T.fetchRequest()
-            do {
-                let fetchResult = try fetchRequest.execute() as! [T]
-                DispatchQueue.main.async {
-                    completionHandler(fetchResult, nil) //->
-                }
-            } catch {
-                DispatchQueue.main.async {
-                    completionHandler(nil, error) //->
-                }
-            }
-        }
-    }
+    //TODO: maybe delete file.  currently unused
+//    static public func fetchAll<T:NSManagedObject>(ofManagedObjectContext moc: NSManagedObjectContext,  completionHandler: @escaping (_ fetchedResult: [T]?, _ error: Error?) -> Void) {
+//        moc.perform {
+//            let fetchRequest = T.fetchRequest()
+//            do {
+//                let fetchResult = try fetchRequest.execute() as! [T]
+//                DispatchQueue.main.async {
+//                    completionHandler(fetchResult, nil) //->
+//                }
+//            } catch {
+//                DispatchQueue.main.async {
+//                    completionHandler(nil, error) //->
+//                }
+//            }
+//        }
+//    }
     
 }
