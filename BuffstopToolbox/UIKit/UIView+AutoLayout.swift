def51e7a7b2949a0f144c1dd50b135c7fc29c27a
Andreas Buff <autobuff@gmx.de>
2016-12-04 16:18:37 +0100

    cosmetic

--- BuffstopToolbox/UIKit/UIView+AutoLayout.swift
+++ BuffstopToolbox/UIKit/UIView+AutoLayout.swift
@@ -39,5 +39,4 @@ public extension UIView {
         superview.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-0-[subview]-0-|", options: .directionLeadingToTrailing, metrics: nil, views: ["subview": self]))
         superview.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-0-[subview]-0-|", options: .directionLeadingToTrailing, metrics: nil, views: ["subview": self]))
     }
-    
 }
