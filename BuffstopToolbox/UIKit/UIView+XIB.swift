091a35c858d623c4a965eb89ce45d543df40c57a
Andreas Buff <autobuff@gmx.de>
2017-01-22 22:10:02 +0100

    adds load from XIB convenience methode

---
+++ BuffstopToolbox/UIKit/UIView+XIB.swift
@@ -0,0 +1,22 @@
+//
+//  UIView+XIB.swift
+//  BuffstopToolbox
+//
+//  Created by Andreas Buff on 22/01/17.
+//  Copyright © 2017 buffhaltestelle. All rights reserved.
+//
+
+import Foundation
+
+extension UIView {
+    class public func fromNib<T : UIView>() -> T? {
+        guard
+            let nibs = Bundle(for: self).loadNibNamed(String(describing: self), owner: nil, options: nil),
+            let nib = nibs.first
+            else {
+                return nil
+        }
+        
+        return nib as? T
+    }
+}
